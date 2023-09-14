274196617d81b40dd6bff963c8d305ddcc364726
VINH\QuangVinh <c4piddd@gmail.com>
2023-09-14 11:12:08 +0700

    update language desktop

--- src/components/layout/header/components/language/index.tsx
+++ src/components/layout/header/components/language/index.tsx
@@ -1,3 +1,4 @@
+import { Dropdown } from "react-bootstrap";
 import "./index.scss";
 
 type Props = {
@@ -5,9 +6,29 @@ type Props = {
 }
 const Language = ({ onClick }: Props) => {
 
-    return <button type="button" className="fn-language" onClick={onClick}>
-        <span>ENG</span>
+    // return <button type="button" className="fn-language" onClick={onClick}>
+    //     <span>ENG</span>
+    //     <img src="/images/icons/language.svg" alt="lang" />
+    // </button>
+
+    return (
+    <Dropdown className="fn-language">
+      <Dropdown.Toggle variant="dark" >
+      <span>ENG</span>
         <img src="/images/icons/language.svg" alt="lang" />
-    </button>
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu>
+        <Dropdown.Item href="#">English</Dropdown.Item>
+        <Dropdown.Item href="#" className="active">Việt Nam</Dropdown.Item>
+        <Dropdown.Item href="#">한국어</Dropdown.Item>
+        <Dropdown.Item href="#">日本語</Dropdown.Item>
+        <Dropdown.Item href="#">Filipino</Dropdown.Item>
+        <Dropdown.Item href="#">हिन्दी</Dropdown.Item>
+        <Dropdown.Item href="#">nigeria</Dropdown.Item>
+        <Dropdown.Item href="#">日本語</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+    )
 }
 export default Language
